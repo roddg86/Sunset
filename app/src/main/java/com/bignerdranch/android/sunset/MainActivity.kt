32af69b167abd0b6881565191d91920785260d34
roddg86 <roddg@mail.ru>
2022-08-28 20:55:51 +0900

    Animation

--- app/src/main/java/com/bignerdranch/android/sunset/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/sunset/MainActivity.kt
@@ -1,11 +1,62 @@
 package com.bignerdranch.android.sunset
 
+import android.animation.ArgbEvaluator
+import android.animation.ObjectAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AccelerateInterpolator
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var sceneView: View
+    private lateinit var sunView: View
+    private lateinit var skyView: View
+
+    /* Загрузка закатных цветов */
+    private val blueSkyColor: Int by lazy {
+        ContextCompat.getColor(this, R.color.blue_sky)
+    }
+    private val sunsetSkyColor: Int by lazy {
+        ContextCompat.getColor(this, R.color.sunset_sky)
+    }
+    private val nightSkyColor: Int by lazy {
+        ContextCompat.getColor(this, R.color.night_sky)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /* Получение ссылок на представления */
+        sceneView = findViewById(R.id.scene)
+        sunView = findViewById(R.id.sun)
+        skyView = findViewById(R.id.sky)
+
+        /* Запуск анимации по нажатию */
+        sceneView.setOnClickListener {
+            startAnimation()
+        }
+    }
+
+    /* Получение верхних координат представлений */
+    private fun startAnimation() {
+        val sunYStart = sunView.top.toFloat()
+        val sunYEnd = skyView.height.toFloat()
+
+        /* Создание анимации солнца */
+        val heightAnimator = ObjectAnimator
+            .ofFloat(sunView, "y", sunYStart, sunYEnd)
+            .setDuration(3000)
+        heightAnimator.interpolator = AccelerateInterpolator() // Добавление ускорения
+
+        /* Анимация цвета неба */
+        val sunsetSkyAnimator = ObjectAnimator
+            .ofInt(skyView, "backgroundColor", blueSkyColor, sunsetSkyColor)
+            .setDuration(3000)
+        sunsetSkyAnimator.setEvaluator(ArgbEvaluator())
+
+        heightAnimator.start()
+        sunsetSkyAnimator.start()
     }
 }
